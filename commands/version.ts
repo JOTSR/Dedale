a8915647cf98b3f532d72f0736367d4bc2506ef3
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-03-26 00:32:00 +0100

    feat: release version command

--- commands/version.ts
+++ commands/version.ts
@@ -1,23 +1,66 @@
-import { Command } from '../deps.ts'
+import { Command, EnumType, releases } from '../deps.ts'
+
+const denoVersions = [
+	...releases.text.matchAll(
+		/###\s(\d+)\.(\d+)\.(\d+)\s\/\s(.*)/g,
+	) as string[][],
+].map((line) => {
+	const [major, minor, patch] = line.slice(1).map((number) =>
+		parseInt(number)
+	)
+	const [date] = line.slice(-1)
+	return { major, minor, patch, date } as const
+})
+
+const latestVersion = denoVersions[0]
 
 export type Version = {
 	list?: boolean
-	install?: string
-	uninstall?: string
+	force?: boolean
+	quiet?: boolean
+	version: string | true
 }
 
 export function versionHandler(
-	{ list, uninstall, install }: Version,
-	versionName: string,
+	{ list, force, quiet, version }: Version,
 ) {
-	throw new Error('Not implemented')
+	if (list) {
+		denoVersions.map(({ major, minor, patch, date }) => {
+			console.log(`v${major}.${minor}.${patch} - ${date}`)
+		})
+		return
+	}
+	if (version) {
+		const command = new Deno.Command(Deno.execPath(), {
+			args: [
+				'upgrade',
+				`${version ? `--version=${version}` : ''}`,
+				`${force ? '--force' : ''}`,
+				`${quiet ? '--quiet' : ''}`,
+			].filter((arg) => arg.length),
+		})
+
+		command.spawn()
+		return
+	}
 }
 
+const version = new EnumType(
+	denoVersions.map(({ major, minor, patch }) => `${major}.${minor}.${patch}`),
+)
+
 export const versionCommand = new Command()
+	.name('deno version')
+	.version('0.1.0')
 	.description('Manage versions of deno executable')
-	.option('-l, --list', 'List installed versions', { default: false })
-	.option('-i, --install [string]', 'Version to install')
-	.option('-u, --uninstall [string]', 'Version to uninstall')
-	.arguments('[string]')
-	//@ts-ignore bad inference
+	.type('version', version)
+	.option('-f, --force', 'Replace current exe even if not out-of-date')
+	.option('-q, --quiet', 'Suppress diagnostic output')
+	.option('-l, --list', 'List installed versions', {
+		conflicts: ['force', 'quiet'],
+	})
+	.option('-v, --version [value:version]', 'Version to install', {
+		default:
+			`${latestVersion.major}.${latestVersion.minor}.${latestVersion.patch}`,
+	})
 	.action(versionHandler)
--- deps.ts
+++ deps.ts
@@ -9,3 +9,5 @@ export { default as dir } from 'https://deno.land/x/dir@1.5.1/mod.ts'
 export { default as username } from 'https://deno.land/x/username@v1.1.1/mod.ts'
 export { DOMParser } from 'https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts'
 export { wait as ConsoleSpinner } from 'https://deno.land/x/wait@0.1.12/mod.ts'
+//@ts-ignore ejm.sh resolve and export default
+export { default as releases } from 'https://ejm.sh/https://raw.githubusercontent.com/denoland/deno/main/Releases.md' assert { type: 'json' }
