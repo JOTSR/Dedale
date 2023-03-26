f0dd7ad8db3df096b5c8085510eca78f2be6a733
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-03-26 18:47:21 +0200

    feat: release module search

--- commands/search.ts
+++ commands/search.ts
@@ -1,18 +1,124 @@
+import { dedale } from '../definitions.ts'
 import { Command } from '../deps.ts'
+import { Module } from 'https://raw.githubusercontent.com/denoland/apiland/1.6.0/types.d.ts'
+
+async function resolvePackages(provider: string, packageName: string) {
+	if (provider === 'deno.land/x') {
+		const results =
+			await (await fetch(`https://deno.land/x?query=${packageName}`))
+				.text()
+		const packages = [...results.matchAll(/<a href=\"\/x\/(\w+)/g)].map((
+			[_, name],
+		) => name).slice(0, 10)
+		for (const moduleName of packages) {
+			const { latest_version, name, description, popularity_score } =
+				await (await fetch(
+					`https://apiland.deno.dev/v2/modules/${moduleName}`,
+				)).json() as Module
+			console.log(
+				`%c${name}%c@${latest_version}%c - ${
+					description.slice(0, 50)
+				}... %c(✨ ${popularity_score})`,
+				'color: green; font-weight: bold',
+				'color: blue; font-weight: bold',
+				'color: white',
+				'color: gray',
+			)
+		}
+		return
+	}
+	if (provider === 'nest.land') {
+		const results = await (await fetch(`https://x.nest.land/api/packages`))
+			.json() as Record<string, string>[]
+		const packages = results.filter(({ name }) =>
+			name.includes(packageName)
+		).slice(0, 10)
+		for (const { latestVersion, description, owner } of packages) {
+			console.log(
+				`%c${latestVersion}%c - ${
+					description.slice(0, 50)
+				}... %c(@${owner})`,
+				'color: green; font-weight: bold',
+				'color: white',
+				'color: gray',
+			)
+		}
+		return
+	}
+	if (provider === 'github.com') {
+		const results = await (await fetch(
+			`https://api.github.com/search/repositories?q=${packageName}`,
+		)).json()
+		const packages = results.items.slice(0, 10)
+		for (const { name, description, score, owner } of packages) {
+			console.log(
+				`%c${name}%c - ${description?.slice(0, 50)}... %c(✨ ${
+					score.toFixed(2)
+				} @${owner.login})`,
+				'color: green; font-weight: bold',
+				'color: white',
+				'color: gray',
+			)
+		}
+		return
+	}
+	if (provider === 'npm') {
+		const results = await (await fetch(
+			`https://registry.npmjs.org/-/v1/search?text=${packageName}`,
+		)).json()
+		for (
+			const { package: pkg } of results.objects.slice(0, 10)
+		) {
+			const { name, version, description, publisher } = pkg
+			console.log(
+				`%c${name}@%c${version}%c - ${
+					description?.slice(0, 50)
+				}... %c(@${publisher?.username})`,
+				'color: green; font-weight: bold',
+				'color: blue; font-weight: bold',
+				'color: white',
+				'color: gray',
+			)
+		}
+		return
+	}
+	if (provider === 'esm.sh') {
+		throw new Error(`search not supported for ${provider}`)
+	}
+	if (provider === 'skypack.dev') {
+		throw new Error(`search not supported for ${provider}`)
+	}
+	if (provider === 'jspm.org') {
+		throw new Error(`search not supported for ${provider}`)
+	}
+	if (provider === 'jsdelivr.com') {
+		throw new Error(`search not supported for ${provider}`)
+	}
+	if (provider === 'unpkg.com') {
+		throw new Error(`search not supported for ${provider}`)
+	}
+	throw new Error(`unsupported provider ${provider}`)
+}
 
 export type Search = {
 	provider?: string
 }
 
-export function searchHandler({ provider }: Search, query: string) {
-	throw new Error('Not implemented')
+export async function searchHandler(
+	{ provider }: Search,
+	query: string,
+) {
+	await resolvePackages(
+		provider ?? dedale.config?.defaultProvider ?? 'deno.land/x',
+		query,
+	)
 }
 
 export const searchCommand = new Command()
 	.description('Search a package')
 	.option(
 		'-p, --provider <url:string>',
-		'Provider to use (ex: https://deno.land/x/',
+		'Provider to use (ex: deno.land/x)',
 	)
 	.arguments('<name:string>')
 	.action(searchHandler)
