b245101fe5ece1b858d77bdc9c7475ddfc035d68
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-03-28 13:53:55 +0200

    feat: release install command

--- commands/install.ts
+++ commands/install.ts
@@ -1,27 +1,55 @@
+import { dedale } from '../definitions.ts'
 import { Command } from '../deps.ts'
+import { ImportMap } from '../utils.ts'
+import { resolvePackages } from './show.ts'
 
 export type Install = {
-	global?: boolean
 	dev?: boolean
 	provider?: string
 }
 
-export function installHandler(
-	{ global, dev, provider }: Install,
-	packageName: string,
+export async function installHandler(
+	{ dev, provider }: Install,
+	...packageNames: string[]
 ) {
-	throw new Error('Not implemented')
+	for (const packageName of packageNames) {
+		try {
+			const { name, path, latest } = await resolvePackages(
+				false,
+				provider ?? dedale.config?.defaultProvider ?? 'deno.land/x',
+				packageName,
+				false,
+			)
+			if (name === undefined || name === 'undefined') {
+				throw new Error(`invalid package name (${name})`)
+			}
+
+			if (path !== latest) {
+				console.warn(
+					`⚠️ A newer version of %c${name}%c is aviable %c${latest}`,
+					'font-weight: bold; color: black; background-color: white',
+					'',
+					'color: green',
+				)
+			}
+			await ImportMap.set({ path, name, kind: dev ? 'dev' : 'asset' })
+			console.log(`✔️ Package ${packageName} successfully installed`)
+		} catch (error) {
+			console.error(`❌ Package ${packageName} not found [${error}]`)
+		}
+	}
 }
 
 export const installCommand = new Command()
-	.description('Install depedency')
-	.option('-g, --global', 'Install dependency globally', { default: false })
-	.option('-d, --dev', 'Install dependency in project deps.dev.ts', {
+	.name('Install depedency')
+	.description('Install dependencies for current project in import map')
+	.version('0.1.0')
+	.option('-d, --dev', 'Install dependency as dev:name', {
 		default: false,
 	})
 	.option(
 		'-p, --provider <url:string>',
-		'Provider to use (ex: https://deno.land/x/',
+		'Provider to use (ex: https://deno.land/x/)',
 	)
-	.arguments('<name:string>')
+	.arguments('<name...:file>')
 	.action(installHandler)
