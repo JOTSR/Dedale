6631d5149bdbb5fc547410c0cda75b57371cf51a
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-03-28 22:41:27 +0200

    feat: release update command

--- commands/update.ts
+++ commands/update.ts
@@ -1,17 +1,54 @@
+import { dedale } from '../definitions.ts'
 import { Command } from '../deps.ts'
+import { ImportMap } from '../utils.ts'
+import { resolvePackages } from './show.ts'
 
 export type Update = {
-	global?: boolean
 	dev?: boolean
+	provider?: string
 }
 
-export function updateHandler({ global, dev }: Update, packageName: string) {
-	throw new Error('Not implemented')
+export async function updateHandler(
+	{ dev, provider }: Update,
+	...packageNames: string[]
+) {
+	if (packageNames[0] === '*') {
+		packageNames = (await ImportMap.get()).map(({ name }) => name)
+	}
+	for (const packageName of packageNames) {
+		try {
+			const { name, latest } = await resolvePackages(
+				false,
+				provider ?? dedale.config?.defaultProvider ?? 'deno.land/x',
+				packageName,
+				false,
+			)
+			if (name === undefined || name === 'undefined') {
+				throw new Error(`invalid package name (${name})`)
+			}
+
+			await ImportMap.set({
+				path: latest,
+				name,
+				kind: dev ? 'dev' : 'asset',
+			})
+			console.log(`✔️ Package ${packageName} successfully installed`)
+		} catch (error) {
+			console.error(`❌ Package ${packageName} not found [${error}]`)
+		}
+	}
 }
 
 export const updateCommand = new Command()
 	.description('Update depedency')
-	.option('-g, --global', 'Update global dependency', { default: false })
+	.description(
+		'Update dependencies for current project in import map, use "*" for update all',
+	)
+	.version('0.1.0')
 	.option('-d, --dev', 'Update dev dependency', { default: false })
-	.arguments('<name:string>')
+	.option(
+		'-p, --provider <url:string>',
+		'Provider to use (ex: https://deno.land/x/)',
+	)
+	.arguments('<name...:string>')
 	.action(updateHandler)
